75d2b4ecca2875ec9442ca70f5feb5972ac16283
Jamie Edge <edgy360@users.noreply.github.com>
2014-05-10 14:28:46 +0100

    Fix relative paths.

--- test/includer-test.js
+++ test/includer-test.js
@@ -6,13 +6,13 @@ var fs = require("fs");
 vows.describe("Including Files").addBatch({
 	"when running the script": {
 		topic: function() {
-			return require("../src/includer.js");
+			return require("src/includer.js");
 		},
 		"it will create an output directory": function() {
-			assert.equal(fs.existsSync("output") && fs.lstatSync("output").isDirectory(), true);
+			assert.equal(fs.existsSync("test/output") && fs.lstatSync("test/output").isDirectory(), true);
 		},
 		"it will create the same number of output files and source files": function() {
-			assert.equal(fs.readdirSync("output").length, fs.readdirSync("source").length);
+			assert.equal(fs.readdirSync("test/output").length, fs.readdirSync("test/source").length);
 		}
 	}
 }).export(module);
